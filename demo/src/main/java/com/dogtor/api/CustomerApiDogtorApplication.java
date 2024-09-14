6a1e6d60ccc0bfd8d5cbb8821378539f680510c8
Gokul <rajgokul9909@gmail.com>
2024-09-14 21:25:58 +0530

    Code Base Commit

---
+++ demo/src/main/java/com/dogtor/api/CustomerApiDogtorApplication.java
@@ -0,0 +1,13 @@
+package com.dogtor.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApiDogtorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApiDogtorApplication.class, args);
+	}
+
+}
